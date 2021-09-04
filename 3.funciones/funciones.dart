01d7baa8bfadb3a43359df0eadcbfbe025244c61
LinaMejia <linalmejia@gmail.com>
2021-09-04 10:49:40 -0500

    funciones

---
+++ 3.funciones/funciones.dart
@@ -0,0 +1,10 @@
+String obtenerNombre(String valor){
+    return 'valor función $valor':
+}
+
+String obtenerNombreFlecha(String valor) => return 'valor función $valor':
+
+void main(){
+    print(obtenerNombre("Lina"));
+    print(obtenerNombreFlecha("Isabel"));
+}
