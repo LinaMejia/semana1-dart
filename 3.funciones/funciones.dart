7b12e89d901baf14603aee92619e6dbb64f6fa6e
LinaMejia <linalmejia@gmail.com>
2021-09-04 10:53:29 -0500

    funciones anonimas

--- 3.funciones/funciones.dart
+++ 3.funciones/funciones.dart
@@ -1,10 +1,34 @@
 String obtenerNombre(String valor){
-    return 'valor función $valor':
+    return 'valor función $valor';
 }
 
-String obtenerNombreFlecha(String valor) => return 'valor función $valor':
+
+
+obtenerNombreFlecha(String valor) => print('valor función $valor');
+
+
+String obtenerNombreNamedParameter({String valor='Lina', String? valor2, required String valor3}){
+    return 'Mi nombre es $valor y $valor2';
+}
+
+parametrosOpcionales(String valor1, [int? valor2]){
+  return  valor1;
+}
 
 void main(){
     print(obtenerNombre("Lina"));
-    print(obtenerNombreFlecha("Isabel"));
+    print(obtenerNombreNamedParameter(valor3: "León"));
+    print(parametrosOpcionales("Lina",23));
+    obtenerNombreFlecha("Isabel");
+  
+  
+    var miFuncionAnonima = (String valor){
+        return valor;
+    };
+  
+    print(miFuncionAnonima("hello"));
+    
+      
+      
+      
 }
