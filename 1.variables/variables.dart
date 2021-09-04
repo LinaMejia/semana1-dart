9cea0fe39deef1464150cf8704fe7994ad489a81
LinaMejia <linalmejia@gmail.com>
2021-09-04 07:58:13 -0500

    variables dart

---
+++ 1.variables/variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  var nombre="Lina";
+  String nombre_dos ="Lina";
+  var edad= 23;
+  int edad_dos = 23;
+  var test;
+  test=100;
+  print(nombre.toUpperCase());
+  print(nombre);
+  print(test);
+}
